d69419c3e0a955ce8fe9258d69574e272eaaef40
spimtav <mrt59@cornell.edu>
2025-12-31 19:17:08 -0500

    chore: add setup script wgets

---
+++ crop_bot/setup.sh
@@ -0,0 +1,4 @@
+wget -f https://raw.githubusercontent.com/Spimtav/gtnh_opencomputers/refs/heads/main/crop_bot/setup.sh
+wget -f https://raw.githubusercontent.com/Spimtav/gtnh_opencomputers/refs/heads/main/crop_bot/.shrc
+wget -f https://raw.githubusercontent.com/Spimtav/gtnh_opencomputers/refs/heads/main/crop_bot/test.lua
+wget -f https://raw.githubusercontent.com/Spimtav/gtnh_opencomputers/refs/heads/main/crop_bot/README.md
