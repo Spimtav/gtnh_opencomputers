224a5d6bbb4580b14cf96a3c591ac73c8946a04e
spimtav <mrt59@cornell.edu>
2026-01-02 20:42:31 -0500

    refactor: abstract patrol code into functions

--- crop_bot/patrol.lua
+++ crop_bot/patrol.lua
@@ -12,7 +12,7 @@ local PLOT_LENGTH = 3
 local PLOT_WIDTH = 3
 local POS_START = {0, 0}
 
-local pos_curr
+local pos_curr = {0, 0}
 
 
 function reset_pos()
@@ -24,38 +24,62 @@ function odd_row(row)
   return (row % 2) == 1
 end
 
-
-reset_pos()
-
-for rounds=1,2 do
-  MOVE.face_dir("N")
-  for y=1,PLOT_LENGTH do
-    if y > 1 then
-      MOVE.travel_dir("N", 1)
-      pos_curr[2] = pos_curr[2] + 1
-      print("At: ("..pos_curr[1]..", "..pos_curr[2]..")")
-    end
-
-    if odd_row(y) then
-      MOVE.face_dir("E")
-    else
-      MOVE.face_dir("W")
-    end
-
-    for x=1,PLOT_WIDTH do
-      BOT.forward()
-      if odd_row(y) do
-        pos_curr[1] = pos_curr[1] + 1
-      else do
-        pos_curr[1] = pos_curr[1] - 1
-      end
-
-      print("At: ("..pos_curr[1]..", "..pos_curr[2]..")")
-    end
+function travel_y(y)
+  if y > 1 then
+    MOVE.travel_dir("N", 1)
+    pos_curr[2] = pos_curr[2] + 1
   end
+ 
+  print_pos()
+end
 
+function travel_x(y)
+  BOT.forward()
+  
+  if odd_row(y) then
+    pos_curr[1] = pos_curr[1] + 1
+  else
+    pos_curr[1] = pos_curr[1] - 1
+  end 
+  
+  print_pos()
+end
+
+function travel_start()
   print("Resetting to charger")
   MOVE.travel_pos(pos_curr, POS_START)
   reset_pos()
 end
+
+function face_inward_x(y)
+  if odd_row(y) then
+    MOVE.face_dir("E")
+  else
+    MOVE.face_dir("W")
+  end
+end
+
+function print_pos() 
+  print("At: ("..pos_curr[1]..", "..pos_curr[2]..")")
+end
+
+
+function main()
+  reset_pos()
+  
+  for rounds=1,2 do
+    for y=1,PLOT_LENGTH do
+      travel_y(y)
+      face_inward_x(y)
+ 
+      for x=1,PLOT_WIDTH do
+        travel_x(y)
+      end
+    end
+  
+    travel_start()
+  end
+end
+
+main()
 
