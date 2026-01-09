0675ba2bdb9e23500a11b30caacf502d860f12c2
spimtav <mrt59@cornell.edu>
2026-01-09 15:52:50 -0500

    feat: global script runner to set project's package paths for child script

---
+++ exec.lua
@@ -0,0 +1,43 @@
+--[[
+Notes:
+- universal entry point for all project scripts
+  - assumes project scripts have a main() method as their entry point
+- loads common configs, constants, and env vars for scripts to use
+]]
+
+
+local shell = require("shell")
+local paths_env_var = "PATHS_LOADED"
+local args = {...}
+
+
+if args[1] == nil then
+  print("Usage: `lua [path/]exec.lua <project-script-name>`")
+  return 1
+end
+
+local script_path = debug.getinfo(2).source:sub(2):match("(.*)/") or "."
+local abs_root = shell.resolve(script_path)
+local project = dofile(abs_root.."/project.lua")
+
+-- Set project paths if not set
+
+if os.getenv(paths_env_var) == nil then
+  os.setenv(paths_env_var, ";")
+end
+local paths_loaded = os.getenv(paths_env_var)
+
+if not string.find(paths_loaded, ";"..abs_root..";", 1, true) then
+  for _,subdir in pairs(project.folders) do
+    package.path = abs_root.."/"..subdir.."/?.lua;"..package.path
+  end
+  package.path = abs_root.."/?.lua;"..package.path
+
+  os.setenv(paths_env_var, paths_loaded..abs_root..";")
+end
+
+-- Execute specified script's main()
+local script_mod = require(args[1])
+
+script_mod.main()
+
---
+++ project.lua
@@ -0,0 +1,38 @@
+--[[
+Notes:
+- hardcoded, manually-updated list of project files and folders
+- unfortunately needed for both fetching and executing of all project files:
+  - fetching: OpenOS has no package manager, so all files must be fetched
+              individually using wget.
+  - exec: Lua can't have recursive, arbitrary paths for module imports, so
+          the exec script needs to know the folder structure to do it manually.
+]]
+
+local project = {}
+
+
+-- put parent directories before their subdirs
+project.folders = {
+  "lib",
+  "lib/common",
+  "lib/robot",
+  "lib/computer",
+  "config",
+  "crop_bot"
+}
+
+project.files = {
+  "README.md",
+  "exec.lua",
+  "fetch.lua",
+  "layout.lua",
+  "lib/common/binary_min_heap.lua",
+  "lib/robot/patrol.lua",
+  "lib/robot/move.lua",
+  "crop_bot/README.md",
+  "crop_bot/cultivate.lua"
+}
+
+
+return project
+
